bb42c6e356e59ad1469e1980b7b2fab1bc676f3a
Tarun <tarun.pstu@gmail.com>
2019-06-16 23:16:49 +0600

    * Connected Modules. * AuthViewModelModule.class is added inside ActivityBuildersModule ,because it will be only necessary   for Authentication only. It would be added inside AppComponent, if it is necessary for       whole application

--- AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
@@ -1,5 +1,6 @@
 package com.tkb.dagger.di;
 
+import com.tkb.dagger.di.auth.AuthViewModelModule;
 import com.tkb.dagger.ui.auth.AuthActivity;
 
 import dagger.Module;
@@ -8,7 +9,13 @@ import dagger.android.ContributesAndroidInjector;
 @Module
 public abstract class ActivityBuildersModule {
 
-    @ContributesAndroidInjector
+    /**
+     * Activity AuthViewModelModule.class is added here ,because it will be only necessary
+     * for Authentication. It would be added inside AppComponent, if it is necessary for
+     * whole application
+     * @return
+     */
+    @ContributesAndroidInjector (modules = {AuthViewModelModule.class})
     abstract AuthActivity contributeAuthActivity();
 
 }
--- AndroidDagger/app/src/main/java/com/tkb/dagger/di/AppComponent.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/AppComponent.java
@@ -12,7 +12,8 @@ import dagger.android.AndroidInjector;
 import dagger.android.support.AndroidSupportInjectionModule;
 
 @Singleton
-@Component (modules = {AndroidSupportInjectionModule.class, ActivityBuildersModule.class,AppModule.class})
+@Component (modules = {AndroidSupportInjectionModule.class,
+        ActivityBuildersModule.class,AppModule.class, ViewModelFactoryModule.class})
 public interface AppComponent extends AndroidInjector<BaseApplication> {
 
     @Component.Builder
