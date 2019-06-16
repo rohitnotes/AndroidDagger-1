187fba3c58ac671aeb31fa57a4455bafd04839f2
Tarun <tarun.pstu@gmail.com>
2019-06-16 23:17:21 +0600

    Added simple log to test

--- AndroidDagger/app/src/main/java/com/tkb/dagger/ui/auth/AuthViewModel.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/auth/AuthViewModel.java
@@ -1,13 +1,17 @@
 package com.tkb.dagger.ui.auth;
 
+import android.util.Log;
+
 import androidx.lifecycle.ViewModel;
 
 import javax.inject.Inject;
 
 public class AuthViewModel extends ViewModel {
 
+    private static final String TAG = "AuthViewModel";
     @Inject
    public AuthViewModel(){
 
+        Log.e(TAG,"ViewModel is working properly");
     }
 }
