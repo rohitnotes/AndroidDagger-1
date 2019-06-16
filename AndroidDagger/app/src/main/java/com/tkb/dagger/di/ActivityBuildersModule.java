7ffa13313773d51e9826052864739c0c2bb0f904
Tarun <tarun.pstu@gmail.com>
2019-06-16 22:30:50 +0600

    * Updated package name

--- AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
@@ -1,9 +1,8 @@
 package com.tkb.dagger.di;
 
-import com.tkb.dagger.AuthActivity;
+import com.tkb.dagger.ui.auth.AuthActivity;
 
 import dagger.Module;
-import dagger.Provides;
 import dagger.android.ContributesAndroidInjector;
 
 @Module
