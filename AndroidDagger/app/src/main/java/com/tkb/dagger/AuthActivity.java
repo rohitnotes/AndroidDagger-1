434ad798abd7122a3c51396ce2da1dc83e5222d8
Tarun <tarun.pstu@gmail.com>
2019-06-16 16:21:19 +0600

    * Loaded image using Glide

--- AndroidDagger/app/src/main/java/com/tkb/dagger/AuthActivity.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/AuthActivity.java
@@ -2,9 +2,12 @@ package com.tkb.dagger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
 
+import com.bumptech.glide.RequestManager;
 import com.tkb.dagger.di.DaggerAppComponent;
 
 import javax.inject.Inject;
@@ -14,13 +17,22 @@ import dagger.android.support.DaggerAppCompatActivity;
 public class AuthActivity extends DaggerAppCompatActivity {
 
     private static final String TAG = "AuthActivity";
+
     @Inject
-    String tarun;
+    RequestManager requestManager;
+
+    @Inject
+    Drawable logo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Log.e(TAG,tarun);
+        setLogo();
+    }
+
+    private void setLogo() {
+        requestManager.load(logo).into((ImageView)findViewById(R.id.login_logo));
     }
 }
