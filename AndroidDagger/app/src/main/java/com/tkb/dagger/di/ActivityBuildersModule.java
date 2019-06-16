4b8aa4460e40b7de3474a560e638f546a06b0c08
Tarun <tarun.pstu@gmail.com>
2019-06-16 14:44:30 +0600

    Added Activity builder module

---
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
@@ -0,0 +1,19 @@
+package com.tkb.dagger.di;
+
+import com.tkb.dagger.AuthActivity;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.android.ContributesAndroidInjector;
+
+@Module
+public abstract class ActivityBuildersModule {
+
+    @ContributesAndroidInjector
+    abstract AuthActivity contributeAuthActivity();
+
+    @Provides
+    static String provideString(){
+        return "Hi Tarun";
+    }
+}
