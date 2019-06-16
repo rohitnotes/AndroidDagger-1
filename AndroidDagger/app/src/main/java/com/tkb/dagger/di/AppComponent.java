5c2e0effda219e11bd055161bfe749ad3a100391
Tarun <tarun.pstu@gmail.com>
2019-06-16 13:47:36 +0600

    Added AppComponent class - This is an interface, which will supply Application instance when necessary.

---
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/AppComponent.java
@@ -0,0 +1,25 @@
+package com.tkb.dagger.di;
+
+import android.app.Application;
+
+import com.tkb.dagger.BaseApplication;
+
+import dagger.BindsInstance;
+import dagger.Component;
+import dagger.android.AndroidInjector;
+import dagger.android.support.AndroidSupportInjectionModule;
+
+@Component (modules = {AndroidSupportInjectionModule.class})
+public interface AppComponent extends AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        @BindsInstance
+        Builder application(Application application);
+
+        AppComponent build();
+
+    }
+
+}
