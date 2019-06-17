c4588e9e202d3e78d74303cc9b308adf4d61a9ab
Tarun <tarun.pstu@gmail.com>
2019-06-17 12:16:45 +0600

    Added CallAdapter factory in Retrofit library. - it will convert object to flowable object

--- AndroidDagger/app/src/main/java/com/tkb/dagger/di/AppModule.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/AppModule.java
@@ -16,6 +16,7 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -28,8 +29,11 @@ public class AppModule {
     @Singleton
     @Provides
     static Retrofit provideRetrofit(){
-        return new Retrofit.Builder().baseUrl(Constants.BASE_URL)
-                .addConverterFactory(GsonConverterFactory.create()).build();
+        return new Retrofit.Builder()
+                .baseUrl(Constants.BASE_URL)
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
     }
     @Singleton
     @Provides
