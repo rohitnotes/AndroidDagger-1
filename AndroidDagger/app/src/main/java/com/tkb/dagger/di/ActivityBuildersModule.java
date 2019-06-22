c81c6ae696259ab34be1373e4dfcceb5431cc194
Tarun <tarun.pstu@gmail.com>
2019-06-23 00:12:30 +0600

    # Added ViewModelModule as the sub module of ActivityModule

--- AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/ActivityBuildersModule.java
@@ -3,6 +3,7 @@ package com.tkb.dagger.di;
 import com.tkb.dagger.di.auth.AuthModule;
 import com.tkb.dagger.di.auth.AuthViewModelModule;
 import com.tkb.dagger.di.main.MainFragmentBuilderModule;
+import com.tkb.dagger.di.main.MainViewModelModule;
 import com.tkb.dagger.ui.auth.AuthActivity;
 import com.tkb.dagger.ui.main.MainActivity;
 
@@ -21,6 +22,7 @@ public abstract class ActivityBuildersModule {
     @ContributesAndroidInjector (modules = {AuthViewModelModule.class, AuthModule.class})
     abstract AuthActivity contributeAuthActivity();
 
-    @ContributesAndroidInjector(modules = {MainFragmentBuilderModule.class})
+    // In every module , two sub module have been added, one for VIew (Activity or Fragment) and another for ViewModel
+    @ContributesAndroidInjector(modules = {MainFragmentBuilderModule.class, MainViewModelModule.class})
     abstract MainActivity contributeMainActivity();
 }
