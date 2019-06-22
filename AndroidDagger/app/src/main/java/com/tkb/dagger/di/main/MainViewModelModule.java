95534c3bb226f984749e06e8fb44fca5d3f2f506
Tarun <tarun.pstu@gmail.com>
2019-06-23 00:11:28 +0600

    # Added ViewModelModule for Profile viewModel

---
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/di/main/MainViewModelModule.java
@@ -0,0 +1,22 @@
+package com.tkb.dagger.di.main;
+
+import androidx.lifecycle.ViewModel;
+
+import com.tkb.dagger.di.ViewModelKey;
+import com.tkb.dagger.ui.auth.AuthViewModel;
+import com.tkb.dagger.ui.main.profile.ProfileFragment;
+import com.tkb.dagger.ui.main.profile.ProfileViewModel;
+
+import dagger.Binds;
+import dagger.Module;
+import dagger.multibindings.IntoMap;
+
+@Module
+public abstract class MainViewModelModule {
+
+    @Binds
+    @IntoMap
+    @ViewModelKey(ProfileViewModel.class)
+    public abstract ViewModel bindMainViewModel(ProfileViewModel profileViewModel);
+
+}
