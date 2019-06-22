a01d611bd9019f3300a87ad764557bd4677a4628
Tarun <tarun.pstu@gmail.com>
2019-06-23 00:13:26 +0600

    # Connected viewModule in ProfileFragment

--- AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/profile/ProfileFragment.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/profile/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.tkb.dagger.ui.main.profile;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,19 +9,32 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
 
 import com.tkb.dagger.R;
+import com.tkb.dagger.di.ViewModelFactoryModule;
+import com.tkb.dagger.viewmodels.ViewModelProviderFactory;
+
+import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
 
 public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
 
+    @Inject
+    ViewModelProviderFactory viewModelProviderFactory;
+    ProfileViewModel profileViewModel;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        Toast.makeText(getActivity(), "Profile Fragment", Toast.LENGTH_SHORT).show();
-
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Log.e(TAG,"Profile ViewModel is connected");
+        profileViewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(ProfileViewModel.class);
     }
 }
