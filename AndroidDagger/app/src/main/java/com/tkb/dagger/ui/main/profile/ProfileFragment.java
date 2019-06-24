56dd20a6466d076078176a42d8c17fd99afd9259
Tarun <tarun.pstu@gmail.com>
2019-06-25 00:03:53 +0600

    # Displayed authenticated users information on profile fragment   - if error , show error message   - if successfully logged in , show email, userId and website

--- AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/profile/ProfileFragment.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/profile/ProfileFragment.java
@@ -5,14 +5,18 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.tkb.dagger.R;
 import com.tkb.dagger.di.ViewModelFactoryModule;
+import com.tkb.dagger.models.User;
+import com.tkb.dagger.ui.auth.AuthResource;
 import com.tkb.dagger.viewmodels.ViewModelProviderFactory;
 
 import javax.inject.Inject;
@@ -22,9 +26,10 @@ import dagger.android.support.DaggerFragment;
 public class ProfileFragment extends DaggerFragment {
     private static final String TAG = "ProfileFragment";
 
+    private TextView email, user, website;
     @Inject
     ViewModelProviderFactory viewModelProviderFactory;
-    ProfileViewModel profileViewModel;
+    private ProfileViewModel profileViewModel;
 
     @Nullable
     @Override
@@ -36,5 +41,49 @@ public class ProfileFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         Log.e(TAG,"Profile ViewModel is connected");
         profileViewModel = ViewModelProviders.of(this,viewModelProviderFactory).get(ProfileViewModel.class);
+
+        email = view.findViewById(R.id.email);
+        user = view.findViewById(R.id.username);
+        website = view.findViewById(R.id.website);
+
+        subcribeObserver();
+    }
+
+    private void subcribeObserver() {
+        /**
+         * Fragment has own lifecycle , so we need to be sure , we have removed previous observer before
+         * assigning new one. It will help to prevent unwanted result
+         */
+        profileViewModel.getAuthenticatedUser().removeObservers(getViewLifecycleOwner());
+        profileViewModel.getAuthenticatedUser().observe(getViewLifecycleOwner(), new Observer<AuthResource<User>>() {
+            @Override
+            public void onChanged(AuthResource<User> authResource) {
+
+                if (authResource != null){
+                    switch (authResource.status){
+                        case AUTHENTICATED:{
+                            setUserDetails(authResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            setErrorDetails(authResource.message);
+                            break;
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+    private void setErrorDetails(String message) {
+        email.setText(message);
+        website.setText("Error!!");
+        user.setText("Error!!");
+    }
+
+    private void setUserDetails(User data) {
+        email.setText(data.getEmail());
+        website.setText(data.getWebsite());
+        user.setText(data.getUsername());
     }
 }
