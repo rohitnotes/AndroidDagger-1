7570dc2d01b9838cba3a34d92b9b060052d5f0c1
Tarun <tarun.pstu@gmail.com>
2019-06-25 19:35:24 +0600

    Initiated Navigation drawer and click listener

--- AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/MainActivity.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/MainActivity.java
@@ -7,28 +7,46 @@ import android.view.MenuItem;
 import android.widget.Switch;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
 import com.tkb.dagger.BaseActivity;
 import com.tkb.dagger.R;
 import com.tkb.dagger.ui.main.post.PostsFragment;
 import com.tkb.dagger.ui.main.profile.ProfileFragment;
 
 
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private static final String TAG = "MainActivity";
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toast.makeText(this,"Successfully logged in",Toast.LENGTH_LONG).show();
-        openFragment();
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        init();
+    }
+    // Setting up navigation controller
+    private void init() {
+        NavController navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout);
+        NavigationUI.setupWithNavController(navigationView,navController);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    void openFragment(){
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, new PostsFragment()).commit();
 
-    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -46,5 +64,22 @@ public class MainActivity extends BaseActivity {
             }
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.nav_profile :{
+                Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.profileScreen);
+                break;
+            }
+            case R.id.nav_posts :{
+                Navigation.findNavController(this,R.id.nav_host_fragment).navigate(R.id.postsScreen);
+                break;
+            }
+        }
+        menuItem.setChecked(true);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
