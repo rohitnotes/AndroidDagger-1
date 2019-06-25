f00d208422ace18cb06d1cbc3d49b90ff466a07b
Tarun <tarun.pstu@gmail.com>
2019-06-25 10:38:13 +0600

    # Added PostsFragment class to display the posts in a recyclerView

---
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/post/PostsFragment.java
@@ -0,0 +1,42 @@
+package com.tkb.dagger.ui.main.post;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.tkb.dagger.R;
+import com.tkb.dagger.viewmodels.ViewModelProviderFactory;
+
+import javax.inject.Inject;
+
+import dagger.android.support.DaggerFragment;
+
+public class PostsFragment extends DaggerFragment {
+    private static final String TAG = "PostsFragment";
+
+    private PostsViewModel postsViewModel;
+    private RecyclerView recyclerView;
+
+    @Inject
+    ViewModelProviderFactory providerFactory;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_posts, container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = view.findViewById(R.id.recycler_view);
+        postsViewModel = ViewModelProviders.of(this,providerFactory).get(PostsViewModel.class);
+
+    }
+}
