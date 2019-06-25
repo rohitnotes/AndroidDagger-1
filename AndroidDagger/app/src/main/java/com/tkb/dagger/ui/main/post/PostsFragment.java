7fe3324cb5026b177bc7fded51e495990509d0e6
Tarun <tarun.pstu@gmail.com>
2019-06-25 15:40:10 +0600

    Connected Post RecyclerView with RecyclerView Adapter , and Bind data with adapter after successful loading

--- AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/post/PostsFragment.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/ui/main/post/PostsFragment.java
@@ -5,21 +5,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tkb.dagger.R;
 import com.tkb.dagger.models.Post;
 import com.tkb.dagger.ui.main.Resource;
+import com.tkb.dagger.util.VerticalSpacingItemDecoration;
 import com.tkb.dagger.viewmodels.ViewModelProviderFactory;
 
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.security.auth.login.LoginException;
 
 import dagger.android.support.DaggerFragment;
 
@@ -28,6 +32,9 @@ public class PostsFragment extends DaggerFragment {
 
     private PostsViewModel postsViewModel;
     private RecyclerView recyclerView;
+
+    @Inject
+    PostsRecyclerAdapter adapter;
 
     @Inject
     ViewModelProviderFactory providerFactory;
@@ -43,6 +50,8 @@ public class PostsFragment extends DaggerFragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         recyclerView = view.findViewById(R.id.recycler_view);
         postsViewModel = ViewModelProviders.of(this,providerFactory).get(PostsViewModel.class);
+
+        initRecyclerView();
         subscribeObserver();
     }
 
@@ -53,10 +62,31 @@ public class PostsFragment extends DaggerFragment {
             public void onChanged(Resource<List<Post>> listResource) {
                 if (listResource !=null ){
                     Log.e(TAG,"onChanges: "+listResource.data);
+                    switch(listResource.status){
+                        case LOADING:{
+                            Log.e(TAG,"Data is loading");
+                            break;
+                        }
+                        case SUCCESS:{
+                            Log.e(TAG,"Successful");
+                            adapter.setPosts(listResource.data);
+                            break;
+                        }
+                        case ERROR:{
+                            Log.e(TAG,"Error loading data");
+                            break;
+                        }
+                    }
                 }
             }
         });
     }
 
+    private void initRecyclerView(){
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        VerticalSpacingItemDecoration itemDecoration = new VerticalSpacingItemDecoration(15);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(adapter);
+    }
 
 }
