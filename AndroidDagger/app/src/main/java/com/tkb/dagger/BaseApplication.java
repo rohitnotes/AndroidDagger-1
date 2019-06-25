30341700b0c40285510223b6b4250f3e2036f154
Tarun <tarun.pstu@gmail.com>
2019-06-25 23:10:32 +0600

    Developed and integrated Custom AuthScope

--- AndroidDagger/app/src/main/java/com/tkb/dagger/BaseApplication.java
+++ AndroidDagger/app/src/main/java/com/tkb/dagger/BaseApplication.java
@@ -5,6 +5,9 @@ import com.tkb.dagger.di.DaggerAppComponent;
 import dagger.android.AndroidInjector;
 import dagger.android.support.DaggerApplication;
 
+/**
+ * This is the entry point of the application and it will allow to inject an Application object
+ */
 public class BaseApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
